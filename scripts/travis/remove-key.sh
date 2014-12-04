a5f1867f8a081da4c5441ab4fbd18e1f09d62ee3
Ajay Sharma <ajay.sharma@v2solutions.com>
2014-12-04 15:37:48 +0530

    Added Certificates & key

---
+++ scripts/travis/remove-key.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  remove-key.sh
+#  XcodeTravis
+#
+#  Created by Ajay Sharma on 12/4/14.
+#  Copyright (c) 2014 ___v2Tech Ventures___. All rights reserved.
+security delete-keychain ios-build.keychain
+rm -f ~/Library/MobileDevice/Provisioning\ Profiles/*
