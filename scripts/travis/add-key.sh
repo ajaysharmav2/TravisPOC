3098dc13947e7e0af5c6ccb6e7d27b97c98074a5
Ajay Sharma <ajay.sharma@v2solutions.com>
2014-12-04 19:19:10 +0530

    YML Formatting

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -7,7 +7,7 @@
 #  Copyright (c) 2014 ___v2Tech Ventures___. All rights reserved.
 security create-keychain -p travis ios-build.keychain
 security import ./scripts/travis/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/travis/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/travis/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/travis/Certificate.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/travis/Certificates-key.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/travis/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
