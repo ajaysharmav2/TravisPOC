97b95953782b55ce7db05b38080023caee870be8
Ajay Sharma <ajay.sharma@v2solutions.com>
2014-12-05 16:01:40 +0530

    Shell Scripts

--- scripts/travis/remove-key.sh
+++ scripts/travis/remove-key.sh
@@ -5,5 +5,6 @@
 #
 #  Created by Ajay Sharma on 12/4/14.
 #  Copyright (c) 2014 ___v2Tech Ventures___. All rights reserved.
+#!/bin/sh
 security delete-keychain ios-build.keychain
-rm -f ~/Library/MobileDevice/Provisioning\ Profiles/*
+rm -f ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision
