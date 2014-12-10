8bcfd347c7494fe158acb123969884def3ba3012
Ajay Sharma <ajay.sharma@v2solutions.com>
2014-12-10 17:45:16 +0530

    New Curl

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -32,9 +32,6 @@ zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dsym.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
-echo "****************"
-echo "Hockey App process beings here"
-echo "****************"
 
 # if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
 #   echo ""
@@ -55,14 +52,14 @@ echo ""
 echo "***************************"
 echo "* Uploading to Hockeyapp  *"
 echo "***************************"
-curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
--F status="2" \
--F notify="0" \
--F notes="$RELEASE_NOTES" \
--F notes_type="0" \
--F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
--F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
-echo "dsym generated"
--H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+curl  \
+-F "status=2" \
+-F "notify=0" \
+-F "notes=$RELEASE_NOTES" \
+-F "notes_type=0" \
+-F "ipa=@$OUTPUTDIR/$APP_NAME.ipa" \
+-F "dsym=@$OUTPUTDIR/$APP_NAME.app.dsym.zip" \
+-H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
+https://rink.hockeyapp.net/api/2/apps/upload
 echo "Upload finish"
 fi
